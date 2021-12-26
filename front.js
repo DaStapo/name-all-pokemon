d5e4a3ece05a1c4aa638e600dcc92b0b33008321
Jan <dahrakl@gmail.com>
2021-12-26 19:46:37 +0100

    panel on/off

--- front.js
+++ front.js
@@ -901,18 +901,10 @@ changeFooterPosition();
 
 
 
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
+document.getElementById("accordion").onclick = function (){
+	if (document.getElementById("panel").style.display == 'none'){
+		document.getElementById("panel").style.display = 'block';
+	}else{	
+		document.getElementById("panel").style.display = 'none'
+	}
 }
