2b6626e6b99e6fa63af171492824b004afc15233
Jan <dahrakl@gmail.com>
2022-05-09 12:31:07 +0200

    index

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let jsonParser = bodyParser.json();
 
 let indexLocation = path.join(__dirname + '/NameAllPokémonQuiz.html');
 let icon = path.join(__dirname + '/favicon.ico');
-let jsLocation = path.join(__dirname + '/front.js');
+let jsLocation = path.join(__dirname + '/obfuscated.js');
 let twitchLocation = path.join(__dirname + '/tmi.min.js');
 let levenshteinLocation = path.join(__dirname + '/levenshtein.js');
 let encodedImgs = path.join(__dirname + '/encoded.js');
