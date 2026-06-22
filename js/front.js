e109afc042f560b248e0d1b66801c47dba532cef
Charon <mitja888@yahoo.de>
2026-06-22 22:43:49 +0200

    lol wrong mega box order

--- js/front.js
+++ js/front.js
@@ -2,7 +2,7 @@ let allLanguages = ['ENG', 'FRE', 'GER', 'ESP', 'ITA', 'KOR', 'JPN', 'CHT', 'CHS
 
 
 let typeList = ["normal", "fire", "water", "grass", "electric", "ice", "ground", "flying", "poison", "fighting", "psychic", "dark", "bug", "rock", "ghost", "dragon", "steel", "fairy"]
-let boxIds = ["big", "kanto", "johto", "hoenn", "sinnoh", "unova", "kalos", "alola", "megakalos", "megahoenn", "unknown", "galar", "gmax", "hisui", "paldea", "paldeadlc", "megalumiose", "megahyperspace", "sub-legendary", "legendary", "mythical", "ultrabeast", "paradox"]
+let boxIds = ["big", "kanto", "johto", "hoenn", "sinnoh", "unova", "kalos", "megakalos", "megahoenn", "alola", "unknown", "galar", "gmax", "hisui", "paldea", "paldeadlc", "megalumiose", "megahyperspace", "sub-legendary", "legendary", "mythical", "ultrabeast", "paradox"]
 let genQuizBoxes = {
     "0": ["kanto", "johto", "hoenn", "sinnoh", "unova", "kalos", "megakalos", "megahoenn", "alola", "unknown", "galar", "gmax", "hisui", "paldea", "paldeadlc", "megalumiose", "megahyperspace"],
     "1": ["kanto"],
